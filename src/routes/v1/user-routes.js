103633961eb2462d7daaee20b8b0897b79e93908
srikrishnastrive <srikrishnamuddangala@gmail.com>
2024-07-17 16:53:40 +0530

    JWT token expired

--- src/routes/v1/user-routes.js
+++ src/routes/v1/user-routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { UserController } = require('../../controllers');
+const {AuthRequestMiddlewares} = require('../../middlewares');
 
 
 
 const userRoutes = express.Router();
 
-userRoutes.post('/signup',UserController.createUser);
-userRoutes.post('/signin',UserController.signIn);
+userRoutes.post('/signup',AuthRequestMiddlewares.validateAuthRequest,UserController.createUser);
+userRoutes.post('/signin',AuthRequestMiddlewares.validateAuthRequest,UserController.signIn);
 
 module.exports = userRoutes;
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -45,9 +45,36 @@ async function signin(data){
 
 }
 
+async  function isAuthenticated(token){
+    try {
+        if(!token){
+            throw new AppError('Missing JWT token',StatusCodes.BAD_REQUEST);
+        }
+        const response = Auth.verifyToken(token);
+        console.log(response);
+        const user = await userRepository.get(response.id);
+        if(!user){
+            throw new AppError('No User found',StatusCodes.NOT_FOUND);
+        }
+        return user.id;
+    } catch (error) {
+        if(error instanceof AppError) throw error;
+        if(error.name == 'JsonWebTokenError'){
+            throw new AppError('Invalid JWT token',StatusCodes.BAD_REQUEST);
+        }
+        if(error.name == 'TokenExpiredError'){
+            throw new AppError('JWT token expired',StatusCodes.BAD_REQUEST);
+        }
+       console.log(error);
+       throw error;
+        
+    }
+}
+
 
 
 module.exports = {
     createUser,
-    signin
+    signin,
+    isAuthenticated
 }
