b7aded8ec84e77686b403a1c1e667ad584d4bbce
srikrishnastrive <srikrishnamuddangala@gmail.com>
2024-07-18 10:56:31 +0530

    added the proxy middlewares

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -6,5 +6,7 @@ module.exports = {
     PORT:  3001,
     SALT_ROUNDS : process.env.SALT_ROUNDS,
     JWT_EXPIRY : process.env.JWT_EXPIRY,
-    JWT_SECRET : process.env.JWT_SECRET
+    JWT_SECRET : process.env.JWT_SECRET,
+    BOOKING_SERVICE : process.env.BOOKING_SERVICE,
+    FLIGHT_SERVICE : process.env.FLIGHT_SERVICE
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const rateLimit = require('express-rate-limit')
+const rateLimit = require('express-rate-limit');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
-const { Auth } = require('./utils/common');
+
 
 const app = express();
 
@@ -17,6 +18,20 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(limiter);
+app.use(
+    '/flightService',
+    createProxyMiddleware({
+      target: ServerConfig.FLIGHT_SERVICE,
+      changeOrigin: true,
+    }),
+  );
+  app.use(
+    '/bookingService',
+    createProxyMiddleware({
+      target: ServerConfig.BOOKING_SERVICE,
+      changeOrigin: true,
+    }),
+  );
 
 app.use('/api', apiRoutes);
 
@@ -24,3 +39,14 @@ app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
    
 });
+
+/**
+ * user
+ *  |
+ * localhost:3001 (API GATEWAY)  localhost:4000/api/v1/bookings-- for authentication 
+ * localhost:3001/bookingsService/api/v1/bookings
+ * localhost:3001/flightService/api/v1/bookings
+ *  |
+ * localhost:3000/api/v1/flights
+ * 
+ */
